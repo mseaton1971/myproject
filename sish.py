7d2fa79f9043c2269a6e72929171ea5f8221e762
Marlon Seaton <mseaton@stevens.edu>
2019-04-26 08:11:04 -0400

    updated sish.py source file to handle some system call errors

--- sish.py
+++ sish.py
@@ -1,6 +1,7 @@
 #sish.py by Marlon Seaton
 import sys
 import os
+import errno
  
 #*Produce error messages for system call failures and other errors
 
@@ -17,15 +18,26 @@ while(line != "exit"):
   #Parse the line into tokens with white space as the delimiter
     
   tokens = line.split()
- 
-  if(os.fork() < 0):
-    ErrorMessage("Forked failed");
   
-  elif os.fork() == 0:
-    os.execvp(tokens[0],tokens)
+  try:
 
+    pid = os.fork()
+ 
+  except OSError as e:
+     print e.strerror   
+#exit("fork failed")
+
+  if pid == 0:
+    
+     try:
+       
+       os.execvp(tokens[0],tokens)
+     
+     except OSError as e:
+        print e.strerror
+       #exit(os.error) #"Failed to exec")
   else:
-    print "in parent"
+    os.waitpid(0,0)
   
   line = raw_input("sish$ ")
 

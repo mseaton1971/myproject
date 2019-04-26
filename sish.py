79b82e6b0ec930409dc726920b8ca63daca2b8a8
mseaton <mseaton@steven.edu>
2019-04-26 14:40:20 -0400

    Added a method called Execute that executes the basic unix commands.

--- sish.py
+++ sish.py
@@ -3,42 +3,42 @@ import sys
 import os
 import errno
  
-#*Produce error messages for system call failures and other errors
+#Method defined to produce error messages for system call failures and other errors
 
 def ErrorMessage(msg):
 
   print msg
   sys.exit(0);
 
+#Method defined to execute basic common unix commands
+def Execute():
 
-line = raw_input("sish$ ")
-
-while(line != "exit"):
-  
-  #Parse the line into tokens with white space as the delimiter
-    
-  tokens = line.split()
-  
-  try:
-
-    pid = os.fork()
- 
-  except OSError as e:
-     print e.strerror   
-#exit("fork failed")
-
-  if pid == 0:
-    
-     try:
-       
-       os.execvp(tokens[0],tokens)
-     
-     except OSError as e:
-        print e.strerror
-       #exit(os.error) #"Failed to exec")
-  else:
-    os.waitpid(0,0)
-  
   line = raw_input("sish$ ")
 
+  while(line != "exit"):
   
+    #Parse the line into tokens with white space as the delimiter
+    
+    tokens = line.split()
+  
+    try:
+      pid = os.fork()
+ 
+    except OSError as e:
+      ErrorMessage(e.strerror)  
+
+    if pid == 0:
+    
+      try:      
+        os.execvp(tokens[0],tokens)
+     
+      except OSError as e:
+	ErrorMessage(e.strerror)
+  
+    else:
+      os.waitpid(0,0)
+  
+    line = raw_input("sish$ ")
+
+# Program execution begins here
+Execute()

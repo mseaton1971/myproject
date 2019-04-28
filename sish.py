d3724124224014123525ffad501fc37ebe653ec5
Marlon Seaton <mseaton@stevens.edu>
2019-04-28 13:29:49 -0400

    added test file and modified source file

--- sish.py
+++ sish.py
@@ -8,15 +8,15 @@ import errno
 def ErrorMessage(msg):
 
   print msg
-  sys.exit(0);
+  isbuiltin = True
+  sys.exit(0)
 
 #This Method supports a few built-in functions if requested by the user
 def Builtins(tokens):
-
-  isbuiltin = False
   
+  isbuiltin = False
+
   if(tokens[0] == "exit"):
-     isbuiltin = True
      sys.exit()
 
   elif(tokens[0] == "cd"):
@@ -24,64 +24,148 @@ def Builtins(tokens):
     if(len(tokens) == 1):
     
       try: 
-        os.chdir('/home') 
+	os.chdir('/home') 
         isbuiltin = True
-        
-      except OSError as e: 
+
+      except OSError in e: 
         ErrorMessage(e.strerror)
     
     else:
-
-
     
       try:
         os.chdir(tokens[1])
-        isbuiltin = True
+	isbuiltin = True
+         
+      except OSError in e:        
+        ErrorMessage(e.strerror)
 
-      except OSError as e:        
-        ErrorMessage(e.strerror) 
-   
-    return isbuiltin
-  
+  return isbuiltin 
+
 
 #Method defined to execute basic common unix commands
 def Execute():
 
-  line = raw_input("sish$ ")
-  
-  isbuiltin = False
- 
   while(True):
   
-    line = raw_input("sish$ ")
     #Parse the line into tokens with white space as the delimiter
-    
+
+    isbuiltin = False
+
+    line = raw_input("sish$ ")
+
+    # Check for redirection commands
+    Redirection(line)
+
     tokens = line.split()
 
-    #Check for built-ins first
     if(Builtins(tokens)):
        continue
-    
     else:
+
+      try:
+        pid = os.fork()
+ 
+      except OSError as e:
+        ErrorMessage(e.strerror)   
+
+      if pid == 0:
     
-       try:
-         pid = os.fork()
-       except OSError as e:
-         print e.strerror   
-
-       if pid == 0:    
-         try:
-           os.execvp(tokens[0],tokens)
-         except OSError as e:
-           print e.strerror
+        try:    
+          os.execvp(tokens[0],tokens)     
+        except OSError as e:
+          ErrorMessage(e.strerror)
        
-       else:
-         os.waitpid(0,0)
-  
- #   line = raw_input("sish$ ")
+      else:
+        os.waitpid(0,0)
 
+#Method used to implement Redirection
+def Redirection(line):
+ 
+   infile = False
+   outfile = False
+   appendfile = False
+
+   #parse the string line using "<, >, or <<" as the delimiter
+
+   if(line.find("<") != -1):
+         tokens = line.split("<")
+   elif(line.find(">") != -1):
+         tokens = line.split(">")
+   elif(line.find(">>") != -1):
+         tokens = line.split(">>")
+   else:
+      return
+   
+   for i in range(len(tokens)):
+      tokens[i] == tokens[i].strip()
+
+   try:
+     pid = os.fork()
+   except OSError as e:
+     ErrorMessage(e.strerror)  
+   
+   if pid == 0:
+
+      for i in range(len(tokens)):
+
+        if(tokens[i] == ">"):
+            outfile = True
+	 
+        if(tokens[i] == "<"):
+	    infile = True
+
+	if(tokens[i] == ">>"):
+	    appendfile = True
+
+        #Redirect output to a file
+        if(outfile):
+		
+	    os.close(sys.stdout.fileno())
+            
+            try:
+	      fdout = os.open(outfile, os.O_WRONLY | os.O_CREAT | os.O_TRUNC,0644)
+	    except OSError as e:
+	       ErrorMessage(e.strerror)
+    
+	    try:
+	      newfdout = os.dup2(fdout,sys.stdout.fileno())
+	    except OSError as e:
+	       ErrorMessage(e.strerror)
+
+	    os.close(fdout)
+
+	elif(infile):
+	
+	   os.close(sys.stdin.fileno());
+	   
+	   try:
+	     fdin = os.open(infile, os.O_RDONLY)
+	   except OSError as e:
+	      ErrorMessage(e.strerror)
+           
+	   try:
+	     newfdin = os.dup2(fdin,sys.stdin.fileno())
+	   except OSError as e:
+	      ErrorMessage(e.strerror)
+
+	   os.close(fdin)
+	
+	elif(appendfile):
+	
+	   os.close(sys.stdout.fileno())
+	   
+	   try:
+	     os.open(appendfile, os.O_WRONLY | os.O_CREAT | os.O_APPEND,0644)
+	   except OSError as e:
+	      ErrorMessage(e.strerror)
+	else:
+	  
+	   try:
+	     os.execvp(tokens[0], tokens)
+	   except OSError as e:
+              ErrorMessage(e.strerror)
+	
 # Program execution begins here
-
 
 print("\n\n\t*********MY SHELL*********")
  
